c1430c09d4b2c7df0afe628ed2d0fb4f58b19564
Martin Bååth <id13mbh@cs.umu.se>
2020-10-07 11:26:25 +0200

    test

--- positionTest.js
+++ positionTest.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', geoFindMe);
-
+const tankLat = 59.5745646;
+const tankLong = 17.840553;
 function geoFindMe() {
   const status = document.querySelector('#status');
   const mapLink = document.querySelector('#map-link');
@@ -14,6 +15,7 @@ function geoFindMe() {
     status.textContent = '';
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
     mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    calculateDistance(latitude,tankLat,longitude,tankLong);
   }
 
   function error() {
@@ -27,4 +29,19 @@ function geoFindMe() {
     navigator.geolocation.watchPosition(success, error);
   }
 
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = (lat2 - lat1).toRad();
+  var dLon = (lon2 - lon1).toRad();
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+          Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+}
+Number.prototype.toRad = function() {
+  return this * Math.PI / 180;
 }
