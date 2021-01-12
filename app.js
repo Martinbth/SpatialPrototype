ec5051de10e9e84cc010b1b5a7f543bfb8441aa3
Martinbth <martinbaath@gmail.com>
2021-01-12 13:11:01 +0100

    test

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ var party = new Howl({
 var dog = new Howl({
   src: ['./sounds/dog.mp3'],
   loop: true,
-  autoplay: true,
+  autoplay: false,
   volume: 1,
 });
 
 var radio = new Howl({
   src: ['./sounds/radio.wav'],
   loop: true,
-  autoplay: true,
+  autoplay: false,
   volume: 1,
 });
 
@@ -29,7 +29,7 @@ var cannon = new Howl({
 var airplane = new Howl({
   src: ['./sounds/airplane.mp3'],
   loop: true,
-  autoplay: false,
+  autoplay: true,
   volume: 1,
 });
 // radio.play();
